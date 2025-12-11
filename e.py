7cb721d2734f6d6e77b7768558562edf09fe169d
awalikar886 <awalikar886@gmail.com>
2025-12-11 14:31:15 +0530

    main file

---
+++ e.py
@@ -0,0 +1,15 @@
+def employee_details(name,emp_id,dept,salary):
+    result=(
+        f"Employee Name:{name}\n"
+        f"Employee id:{id}\n"
+        f"Department:{dept}\n"
+        f"Salary:{salary}\n"
+    )
+    return result
+
+if__name__="__main__":
+    name="Akash"
+    emp_id=141
+    dept="IT"
+    salary=87000
+print(employee_details(name,emp_id,dept,salary))
