341104c0a813f2e06e1fb6199c4c7abaa9433b4c
awalikar886 <awalikar886@gmail.com>
2025-12-11 14:51:36 +0530

    Fix test function name and expected output format

--- test_e.py
+++ test_e.py
@@ -1,10 +1,11 @@
 from e import employee_details
 
-def test_emplyee_details():
-    expected output=(
-        "Employee Name:Akash"
-        "Employee id: 141"
-        "dept:IT"
-        "salary:87000"
+def test_employee_details():
+    expected_output = (
+        "Employee Name: Akash\n"
+        "Employee ID: 141\n"
+        "Department: IT\n"
+        "Salary: 87000"
     )
-    assert employee_details("Akash","141","IT",87000)==expected output
+    
+    assert employee_details("Akash", "141", "IT", 87000) == expected_output
