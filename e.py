cc1a8647155c2280d22e4cd3d31d9c1bef25993a
awalikar886 <awalikar886@gmail.com>
2025-12-11 14:52:48 +0530

    Fix formatting and variable name in employee_details

--- e.py
+++ e.py
@@ -1,15 +1,16 @@
-def employee_details(name,emp_id,dept,salary):
-    result=(
-        f"Employee Name:{name}\n"
-        f"Employee id:{id}\n"
-        f"Department:{dept}\n"
-        f"Salary:{salary}\n"
+def employee_details(name, emp_id, dept, salary):
+    result = (
+        f"Employee Name: {name}\n"
+        f"Employee ID: {emp_id}\n"
+        f"Department: {dept}\n"
+        f"Salary: {salary}"
     )
     return result
 
- if __name__ == "__main__":
-    name="Akash"
-    emp_id=141
-    dept="IT"
-    salary=87000
-print(employee_details(name,emp_id,dept,salary))
+
+if __name__ == "__main__":
+    name = "Akash"
+    emp_id = 141
+    dept = "IT"
+    salary = 87000
+    print(employee_details(name, emp_id, dept, salary))
